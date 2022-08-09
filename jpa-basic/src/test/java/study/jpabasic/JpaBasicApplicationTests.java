abffd9cabc7b16996701db3ec5e302d435bc592e
player-geun <player_geun@naver.com>
2022-08-09 23:26:13 +0900

    init #1 - project start
    프로젝트 시작을 위한 셋팅을 진행한다.

---
+++ jpa-basic/src/test/java/study/jpabasic/JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
